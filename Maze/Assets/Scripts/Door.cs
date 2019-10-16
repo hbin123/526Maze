8bbd69a6ed5e2403f35bda44fbadce642c15ec98
Yin-Hsia Yen <elsieyen@yin-hsias-mbp.home>
2019-10-16 15:54:21 -0700

    automatic door open

---
+++ Maze/Assets/Scripts/Door.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    Transform tf;
+    private bool isOpen = false; 	
+    // Start is called before the first frame update
+    void Start()
+    {
+    	tf = gameObject.GetComponent<Transform>();       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OpenDoorMethod()
+    {
+        tf.Rotate(Vector3.up, 90);
+        Debug.Log("open");
+        isOpen = true;
+
+    }
+    public void CloseDoorMethod()
+    {
+        tf.Rotate(Vector3.up, -90);
+        Debug.Log("close");
+        isOpen = false;
+    }
+
+    public bool GetIsOpen()
+    {
+        return isOpen;
+    }
+    public void SetIsOpen(bool b)
+    {
+        isOpen = b;
+    }    
+}
---
+++ Maze/Assets/Scripts/Trigger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+	private Door m_Door;
+    private bool enterCollide = false;
+    private int counter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Door = GameObject.Find("DoorShaft1").GetComponent<Door>();
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("enter: " + collider.name);
+        enterCollide = true;
+        m_Door.OpenDoorMethod();
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        Debug.Log("exit");
+        enterCollide = false;
+        m_Door.CloseDoorMethod();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Update: " + enterCollide);
+
+        //if (enterCollide)
+        //{
+        //    //Debug.Log("Update: " + m_Door.GetIsOpen());
+
+        //    if (Input.GetKeyUp(KeyCode.F))
+        //    {
+        //        if (m_Door.GetIsOpen())
+        //        {
+        //            m_Door.CloseDoorMethod();
+
+        //        }
+        //        else
+        //        {
+        //            m_Door.OpenDoorMethod();
+        //        }
+        //    }
+        //}
+    }
+}
