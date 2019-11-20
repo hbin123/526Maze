15b49b468977b398bb9050b9658e3027d3ad1209
LAPTOP-66KD7RLM\Leax <xli932@usc.edu>
2019-11-19 16:44:24 -0800

    MemoryGame

---
+++ Maze/Assets/Scripts/Card.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    public static bool DO_NOT_FLIP = false;
+
+    [SerializeField]
+    private int state;
+    [SerializeField]
+    private int cardValue;
+    [SerializeField]
+    private bool initialized = false;
+
+    private Sprite cardBack;
+    private Sprite cardFace;
+
+    private GameObject manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = 1;
+        manager = GameObject.FindGameObjectWithTag("MemoryGameManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setupGraphics()
+    {
+        cardBack = manager.GetComponent<MemoryGameManager>().getCardBack();
+        cardFace = manager.GetComponent<MemoryGameManager>().getCardFace(cardValue);
+
+        flipCard();
+    }
+    
+    public void flipCard()
+    {
+        if(state == 0)
+        {
+            state = 1;
+        }
+        else if(state == 1)
+        {
+            state = 0;
+        }
+
+        if(state == 0 && !DO_NOT_FLIP)
+        {
+            GetComponent<Image>().sprite = cardBack;
+        }
+        else if (state == 1 && !DO_NOT_FLIP)
+        {
+            GetComponent<Image>().sprite = cardFace;
+        }
+    }
+
+    public int CardValue
+    {
+        get { return cardValue; }
+        set { cardValue = value; }
+    }
+
+    public int State
+    {
+        get { return state; }
+        set { state = value; }
+    }
+
+    public bool Initialized
+    {
+        get { return initialized; }
+        set { initialized = value; }
+    }
+
+    public void falseCheck()
+    {
+        StartCoroutine(pause());
+    }
+    IEnumerator pause()
+    {
+        yield return new WaitForSeconds(1);
+        if(state == 0)
+        {
+            GetComponent<Image>().sprite = cardBack;
+        }
+        else if(state == 1)
+        {
+            GetComponent<Image>().sprite = cardFace;
+        }
+        DO_NOT_FLIP = false;
+    }
+}
---
+++ Maze/Assets/Scripts/MemoryGameManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MemoryGameManager : MonoBehaviour
+{
+    public Sprite[] cardFace;
+    public Sprite cardBack;
+    public GameObject[] cards;
+
+    private bool init = false;
+    private int matches = 6;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!init)
+        {
+            initializeCards();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            checkCards();
+        }
+    }
+
+    void initializeCards()
+    {
+        for(int id = 0; id<2; id++)
+        {
+            for(int i=1; i<=6; i++)
+            {
+                bool test = false;
+                int choice = 0;
+                while (!test)
+                {
+                    choice = Random.Range(0, cards.Length);
+                    test = !(cards[choice].GetComponent<Card>().Initialized);
+                }
+                cards[choice].GetComponent<Card>().CardValue = i;
+                cards[choice].GetComponent<Card>().Initialized = true;
+            }
+        }
+
+        foreach (GameObject c in cards)
+        {
+            c.GetComponent<Card>().setupGraphics();
+        }
+        if (!init)
+        {
+            init = true;
+        }
+    }
+
+    public Sprite getCardBack()
+    {
+        return cardBack;
+    }
+    public Sprite getCardFace(int i)
+    {
+        return cardFace[i-1];
+    }
+
+    void checkCards()
+    {
+        List<int> c = new List<int>();
+        for(int i=0; i<cards.Length; i++)
+        {
+            if(cards[i].GetComponent<Card>().State == 1)
+            {
+                c.Add(i);
+            }
+        }
+        if(c.Count == 2)
+        {
+            cardComparison(c);
+        }
+    }
+
+    void cardComparison(List<int> c)
+    {
+        Card.DO_NOT_FLIP = true;
+        int x = 0;
+        
+        if(cards[c[0]].GetComponent<Card>().CardValue == cards[c[1]].GetComponent<Card>().CardValue)
+        {
+            x = 2;
+            matches--;
+            if(matches == 0)
+            {
+                //success
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+        
+        for(int i=0; i<c.Count; i++)
+        {
+            cards[c[i]].GetComponent<Card>().State = x;
+            cards[c[i]].GetComponent<Card>().falseCheck();
+        }
+    }
+}
